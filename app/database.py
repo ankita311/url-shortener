e080ab53546e23704adc361920dc23794c2ba1bd
Ankita Verma <ankita.av.934@gmail.com>
2025-06-09 00:15:04 +0530

    database url

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ from .config import settings
 #sqlalchemy url format
 #'postgresql://<username>:<password>@<ip-address/hostname>/<databasename>
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = f'{settings.database_url}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
